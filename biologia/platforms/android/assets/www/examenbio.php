f46658e083b181cb55107f7f421fb5811afb760b
Luis Ernesto Garcia Medina <ernesto.r.2.em@gmail.com>
2015-12-17 12:17:51 -0600

    Agrego aplicacion de Biologia

---
+++ biologia/platforms/android/assets/www/examenbio.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="esp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">    
+    <meta name="viewport" content="width=960px, maximum-scale=1.0, user-scalable=yes"/>
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="StyleSheet" href="estilos.css" type="text/css">
+    <script type="text/javascript" src="dist/js/jquery.js"></script>
+    <script type="text/javascript" src="dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="contenedor">
+			<div class="row">
+				<div class="menu">
+	  				<div class="menuizq col-xs-12 col-lg-12 imagenmenu" >
+	  					<a class="menuizq" href="index.html"><img src="img/LogoHeader.png"></a>
+	  					<span class="separador" > </span>
+	  					<div class="materia">
+	  						Ciencias primer grado: Biología
+	  					</div>
+	  						<div  style="float:right">
+		  					<a class="menuder" href="#"><img src="img/icono4.png"></a>
+		  					<a class="menuder" href="#"><img src="img/icono3.png"></a>
+		  					<a class="menuder" href="#"><img src="img/icono2.png"></a>
+		  					<span class="separador" > </span>
+		  					<a class="menuder" href="http://apprende.mx/"><img src="img/icono1.png"></a>
+		  				</div>
+					</div>
+	  			</div>
+			</div>
+			<div class="row">			
+				<div class="ruta col-xs-12">Ciencias primer grado: Biología	/ Bloque II. La nutrición como base para la salud y la vida	/ Tema 3. Interacciones entre la ciencia y la tecnología en la satisfacción de las necesidades e intereses	/ Contenido: Valoración de la importancia de las iniciativas en el marco del Programa de las Naciones Unidas para el Medio Ambiente a favor del desarrollo sustentable. / Sesión 9</div>				
+			</div>
+			<div class="row datauser">
+				<div id="usuario">
+	            Alumno :      </div>
+			</div>		
+			<div class="row">
+				<div class="logo col-xs-12">				
+					<div class="text-center header-image">
+						<a><img src="img/iconBIO.png"></a>
+					</div>				
+					
+				</div>
+			</div>
+		<form  action="procesarbio.php" method="post" enctype="multipart/form-data" accept-charset="utf-8" >
+		<div class="row">
+			<div class="col-xs-3 infoPregunta">
+				<div class="info">
+					<h5 class="no">Pregunta <span>1</span></h5>
+					<div class="state" >Sin responder aún</div>
+				</div>
+		</div>
+			<div class="col-xs-9 Pregunta">
+				<div class="formulario">
+			<div class="qtext"><p>Lee con atención y relaciona las columnas</p></div><div class="ablock">
+			<table class="answer">
+			<tbody>
+			<tr class="r0">
+			<td class="text"><p>En ella se busca redefinir el Protocolo de Kioto</p></td>
+			<td class="control">
+			<select id="menuq14118:1_sub0" class="select menuq14118:1_sub0" name="q14118:1_sub0">
+			<option selected="selected" value="0">Elegir...</option>
+			<option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option>
+			<option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option>
+			<option value="4">Koffi Annan</option><option value="5">1968</option>
+			<option value="6">2000</option><option value="7">1997</option>
+			<option value="8">Informe Global 2000</option>
+			<option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option>
+			</select>
+			 </td></tr><tr class="r1"><td class="text"><p>Países que se niegan a cumplir compromisos&nbsp; del Protocolo de Kioto.</p></td>
+			 <td class="control"><select id="menuq14118:1_sub1" class="select menuq14118:1_sub1" name="q14118:1_sub1"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr><tr class="r0"><td class="text"><p>En ella se dio un nuevo ímpetu a la lucha contra la pobreza y la protección al medio ambiente</p></td><td class="control"><select id="menuq14118:1_sub2" class="select menuq14118:1_sub2" name="q14118:1_sub2"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr><tr class="r1"><td class="text"><p>Año de la Primera Conferencia de Ciudades Europeas Sostenibles</p></td><td class="control"><select id="menuq14118:1_sub3" class="select menuq14118:1_sub3" name="q14118:1_sub3"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr><tr class="r0"><td class="text"><p>En este documento se concluye que la biodiversidad es un&nbsp; factor crítico para el adecuado funcionamiento del planeta.</p></td><td class="control"><select id="menuq14118:1_sub4" class="select menuq14118:1_sub4" name="q14118:1_sub4"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr><tr class="r1"><td class="text"><p>Lanzamiento de la Carta de la Tierra</p></td><td class="control"><select id="menuq14118:1_sub5" class="select menuq14118:1_sub5" name="q14118:1_sub5"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr><tr class="r0"><td class="text"><p>Señala que el medioambiente a nivel mundial continúa siendo vulnerable y que la degradación de los ecosistemas parece no tener retroceso.</p></td><td class="control"><select id="menuq14118:1_sub6" class="select menuq14118:1_sub6" name="q14118:1_sub6"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr><tr class="r1"><td class="text"><p>Año en&nbsp; el que el Club de Roma convocó para analizar las grandes modificaciones&nbsp; que estaba sufriendo el ambiente</p></td><td class="control"><select id="menuq14118:1_sub7" class="select menuq14118:1_sub7" name="q14118:1_sub7"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr><tr class="r0"><td class="text"><p>En ella nace la Agenda 21</p></td><td class="control"><select id="menuq14118:1_sub8" class="select menuq14118:1_sub8" name="q14118:1_sub8"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr><tr class="r1"><td class="text"><p>Año en el que se aprueba el Protocolo de Kioto</p></td><td class="control"><select id="menuq14118:1_sub9" class="select menuq14118:1_sub9" name="q14118:1_sub9"><option selected="selected" value="0">Elegir...</option><option value="1">Conferencia de la ONU sobre Medio Ambiente y Desarrollo Sostenible.</option><option value="2">Cumbre de Bali</option><option value="3">Estados Unidos y China</option><option value="4">Koffi Annan</option><option value="5">1968</option><option value="6">2000</option><option value="7">1997</option><option value="8">Informe Global 2000</option><option value="9">Conferencia Mundial sobre Desarrollo Sostenible Río + 10.</option><option value="10">1994</option></select> </td></tr></tbody></table></div>
+
+			</div>
+		</div>
+	</div>
+<br>
+
+<br>
+<div class="submitbtns"  style="margin-left:50%"id="yui_3_9_1_2_1423092779462_369"><input type="submit" name="next" value="Enviar y Terminar" id="yui_3_9_1_2_1423092779462_368"></div>
+	<input type="hidden" name="inicio"  <?php 
+						   $arrayMeses = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+   'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
+ 
+   $arrayDias = array( 'Domingo', 'Lunes', 'Martes',
+       'Miercoles', 'Jueves', 'Viernes', 'Sabado');
+     
+    echo "value='".$arrayDias[date('w')].", ".date('d')." de ".$arrayMeses[date('m')-1]." de ".date('Y')." ".date('h:i:s')."'";
+
+ ?>>
+
+</form>
+		<br><br><br>
+	</div>
+		</body>
+</html>
